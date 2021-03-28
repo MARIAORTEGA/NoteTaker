0291ccc167fd50672cddc43bfbf2918127f0b12a
Maria Ortega <mariaortegapb@gmail.com>
2021-03-28 13:47:46 -0400

    redifined folder names

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,23 +1,22 @@
-var tableData = require("Develop/db/db.json");
+var newNote = require("../db/db");
 
-module.exports = function(app) {
-    app.get("/api/notes", function(req, res) {
-        res.json(db);
-      });
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    res.json(db);
+  });
 
-      app.post("/api/notes", function(req, res) {
-    //     if (tableData.length < 5) {
-    //         tableData.push(req.body);
-    //         res.json(true);
-    //       }
-    //       else {
-    //         waitListData.push(req.body);
-    //         res.json(false);
-    //       }
-    //     });
-    //     res.json(db);
-    //   });
-    
-      app.delete("/api/notes/:id", function(req, res) {
-        res.json(db);
-      });
+  //what is newNote?
+  app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    console.log(newNote);
+    newNote.push(newNote);
+    res.json(newNote);
+  });
+
+  app.delete("/api/notes/:id", function (req, res) {
+    var deleteNote = "id" + req.params.id;
+    console.log(deleteNote);
+    deleteNote.push(deleteNote);
+    res.json(deleteNote);
+  });
+};
