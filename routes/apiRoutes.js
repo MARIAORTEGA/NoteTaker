1ac3f6a1a5fbe3bfd4f7c97074d1673322f89087
Maria Ortega <mariaortegapb@gmail.com>
2021-04-12 10:50:35 -0400

    changed api request to /api/

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,14 +2,14 @@ var db = require("../db/db");
 var fs = require("fs");
 const router = require("express").Router();
 
-router.get("//notes", function (req, res) {
+router.get("/api/notes", function (req, res) {
   fs.readFile(__dirname + "../db/db.json", function (err, data) {
     if (err) throw err;
     res.json(JSON.parse(db));
   });
 });
 
-router.post("//notes", function (req, res) {
+router.post("/api/notes", function (req, res) {
   fs.readFile(path.join);
   fs.writeFile(
     path.join(__dirname, "/db/db.json"),
@@ -23,7 +23,7 @@ router.post("//notes", function (req, res) {
   );
 });
 
-router.delete("//notes/:id", function (req, res) {
+router.delete("/api/notes/:id", function (req, res) {
   var deleteNote = "id" + req.params.id;
   console.log(deleteNote);
   deleteNote.push(deleteNote);
