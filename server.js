46c55936f3e5b396dee54d66b9d89bc371f4ecbd
Maria Ortega <mariaortegapb@gmail.com>
2021-04-12 11:18:20 -0400

    changed api routes

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use("/", apiRoutes);
-app.use("/", htmlRoutes);
+app.use("api", apiRoutes);
+app.use("", htmlRoutes);
 
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
