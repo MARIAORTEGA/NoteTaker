7bbe1abdf30f537b27732e77d7a338bb72468aa6
Maria Ortega <mariaortegapb@gmail.com>
2021-04-12 12:07:50 -0400

    removed router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,29 @@
-var db = require("../db/db");
+var db = require("../db/db.json");
 var fs = require("fs");
-const router = require("express").Router();
 
-router.get("/api/notes", function (req, res) {
-  fs.readFile(__dirname + "../db/db.json", function (err, data) {
-    if (err) throw err;
-    res.json(JSON.parse(db));
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    res.json(db);
   });
-});
 
-router.post("/api/notes", function (req, res) {
-  fs.readFile(path.join);
-  fs.writeFile(
-    path.join(__dirname, "/db/db.json"),
-    JSON.stringify(newNote),
-    (err) => {
-      var newNote = req.body;
-      console.log(newNote);
-      newNote.push(newNote);
-      res.json(newNote);
-    }
-  );
-});
+  app.post("/api/notes", function (req, res) {
+    db.push(req.body);
+    db.forEach((note, notes) => {
+      note.id = notes + 1;
+    });
+    fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+      res.json(db);
+    });
+  });
 
-router.delete("/api/notes/:id", function (req, res) {
-  var deleteNote = "id" + req.params.id;
-  console.log(deleteNote);
-  deleteNote.push(deleteNote);
-  res.json(deleteNote);
-});
-
-module.exports = router;
+  app.delete("/api/notes/:id", function (req, res) {
+    var id = req.params.id;
+    db.splice(id - 1, 1);
+    db.forEach((note, notes) => {
+      note.id = notes + 1;
+    });
+    fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+      res.json(db);
+    });
+  });
+};
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express = require("express");
-const htmlRoutes = require("./routes/htmlRoutes");
-const apiRoutes = require("./routes/apiRoutes");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use("api", apiRoutes);
-app.use("", htmlRoutes);
+app.use(express.static(__dirname + "/public"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
